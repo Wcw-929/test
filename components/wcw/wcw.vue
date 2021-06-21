b3239a70e5972c3f2dcba23b0654f259ec93ed4c
wcw <1085248665@qq.com>
2021-06-21 20:12:15 +0800

    first commit

---
+++ components/wcw/wcw.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<view class="article">
+			<view class="article-name" v-for="item in cats":key='item.id'>
+				{{item.catname}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:['cats'],
+		name:"wcw",
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<wcw :cats="cats"></wcw>
+		</view>
+	</view>
+</template>
+
+<script>
+	import wcw from '@/components/wcw/wcw.vue'
+	export default {
+		data() {
+			return {
+				cats:[]
+			}
+		},
+		onLoad() {
+			uni.request({
+				url:'http://localhost:7001/cats',
+				success: (res) => {
+					this.cats = res.data
+				}
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #333333;
+	}
+</style>
